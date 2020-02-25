dbe385bfb635342adbb88ae8964e1bdfd3942b1a
Tuyen Dang <tuyendq@gmail.com>
2020-02-25 16:42:08 +0700

    implement graphs endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,38 @@ router.get('/', function(req, res, next) {
 /**
  * Get VM status
  */
+router.get('/:id/status', function(req, res, next) {
+  const https = require('https');
+  const options = {
+    hostname: 'api.cloudcone.com',
+    port: 443,
+    path: '/api/v1/compute/:' + req.params.id + '/status',
+    method: 'GET',
+    headers: {'App-Secret': process.env.API_KEY, 'Hash': process.env.API_HASH}
+  };
+
+  const request = https.request(options, (response) => {
+    console.log('statusCode:', response.statusCode);
+    console.log('headers:', response.headers);
+
+    response.on('data', (d) => {
+      //process.stdout.write(d);
+      console.log(d.toString());
+      res.status(response.statusCode).send(d.toString());
+    });
+  });
+
+  request.on('error', (e) => {
+    console.error(e);
+  });
+  request.end();
+
+});
+
+
+/**
+ * Get VM info
+ */
 router.get('/:id/info', function(req, res, next) {
     const https = require('https');
     const options = {
@@ -37,7 +69,36 @@ router.get('/:id/info', function(req, res, next) {
     });
     request.end();
 
-  // res.json({message: "Check VM status"});
+});
+
+/**
+ * Get VM graphs
+ */
+router.get('/:id/graphs', function(req, res, next) {
+  const https = require('https');
+  const options = {
+    hostname: 'api.cloudcone.com',
+    port: 443,
+    path: '/api/v1/compute/' + req.params.id + '/graphs',
+    method: 'GET',
+    headers: {'App-Secret': process.env.API_KEY, 'Hash': process.env.API_HASH}
+  };
+
+  const request = https.request(options, (response) => {
+    console.log('statusCode:', response.statusCode);
+    console.log('headers:', response.headers);
+
+    response.on('data', (d) => {
+      //process.stdout.write(d);
+      console.log(d.toString());
+      res.status(response.statusCode).send(d.toString());
+    });
+  });
+
+  request.on('error', (e) => {
+    console.error(e);
+  });
+  request.end();
 
 });
 
