57588675f2a0da3ce2e770da4ac2e4b148acd1f5
Tuyen Dang <tuyendq@gmail.com>
2020-02-25 15:58:46 +0700

    /api/:id/info

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const dotenv = require('dotenv').config();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Explore CloudCone API' });
+});
+
+/**
+ * Get VM status
+ */
+router.get('/:id/info', function(req, res, next) {
+    const https = require('https');
+    const options = {
+      hostname: 'api.cloudcone.com',
+      port: 443,
+      path: '/api/v1/compute/' + req.params.id + '/info',
+      method: 'GET',
+      headers: {'App-Secret': process.env.API_KEY, 'Hash': process.env.API_HASH}
+    };
+
+    const request = https.request(options, (response) => {
+      console.log('statusCode:', response.statusCode);
+      console.log('headers:', response.headers);
+
+      response.on('data', (d) => {
+        res.json(d);
+        process.stdout.write(d);
+      });
+    });
+
+    request.on('error', (e) => {
+      console.error(e);
+    });
+    request.end();
+
+  // res.json({message: "Check VM status"});
+
 });
 
 module.exports = router;
